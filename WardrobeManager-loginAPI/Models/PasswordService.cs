1ecd9f502d59a82646200096ea5f09d501d1dca8
rlenerts <114050583+rlenerts@users.noreply.github.com>
2025-02-18 19:22:07 +0200

    Update PasswordService.cs

--- WardrobeManager-loginAPI/Models/PasswordService.cs
+++ WardrobeManager-loginAPI/Models/PasswordService.cs
@@ -9,10 +9,10 @@
         {
             using (SHA256 sha256Hash = SHA256.Create())
             {
-                // Pārvērš paroļu tekstu par bajtiem
+               
                 byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(password));
 
-                // Pārvērš bajtus par hex string
+                
                 StringBuilder builder = new StringBuilder();
                 for (int i = 0; i < bytes.Length; i++)
                 {
